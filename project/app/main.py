139844ee355462e42514951b1fbcf49a11e8969c
MahmoudAbderahman <mahmoud.abou-auf@outlook.com>
2021-10-13 09:08:44 +0200

    postgres

--- project/app/main.py
+++ project/app/main.py
@@ -1,10 +1,20 @@
 # project/app/main.py
+import os
 
 from fastapi import FastAPI, Depends
+from tortoise.contrib.fastapi import register_tortoise
 
 from .config import Settings, get_settings
 
 app = FastAPI()
+
+register_tortoise(
+    app,
+    db_url=os.environ.get("DATABASE_URL"),
+    modules={"models": ["app.models.tortoise"]},
+    generate_schemas=False,
+    add_exception_handlers=True,
+)
 
 @app.get("/ping")
 async def pong(settings: Settings = Depends(get_settings)):
